0d027b73a2ce085051450851fda93d82f116fac8
Ankita <ankita.kumari@memeinfotech.com>
2018-04-27 15:52:11 +0530

    populating bills

--- service/billService.js
+++ service/billService.js
@@ -10,47 +10,51 @@ var Bill = require('../models/bill');
  * no response value expected for this operation
  **/
 exports.createBill = function (body) {
-  return new Promise(function (resolve, reject) {
-    var bill = new Bill(body);
-    bill.createdBy= body.userId;
-    bill.updatedBy= body.userId;
-    bill.createdDate= Date.now();
-    bill.updatedDate= Date.now();    
-    bill.save(function (err, bill) {
-      if (err) {
-        // if (err.errors && err.errors.email)  // error check if email is not present
-        // {
-        //   reject({ error: true, "message": "Email is required to create a client" })
-        // }
-        // else if (err.code == 11000) // error check if email is not unique
-        // {
-        //   reject({ error: true, "message": "This email already exists" })
-        // }
-        // else
-          reject({ error: true, message: err });
+    return new Promise(function (resolve, reject) {
+        var bill = new Bill(body);
+        bill.createdBy = body.userId;
+        bill.updatedBy = body.userId;
+        bill.createdDate = Date.now();
+        bill.updatedDate = Date.now();
+        bill.save(function (err, bill) {
+            if (err) {
+                // if (err.errors && err.errors.email)  // error check if email is not present
+                // {
+                //   reject({ error: true, "message": "Email is required to create a client" })
+                // }
+                // else if (err.code == 11000) // error check if email is not unique
+                // {
+                //   reject({ error: true, "message": "This email already exists" })
+                // }
+                // else
+                reject({ error: true, message: err });
 
-        return;
-      }
-      resolve({ error: false, result: bill });
-    })
+                return;
+            }
+            resolve({ error: false, result: bill });
+        })
 
-  });
+    });
 }
 
 exports.getBill = function (id) {
-  return new Promise(function (resolve, reject) {
-    // let perPage = parseInt(limit) ? parseInt(limit) : 10;
-    // let pageCount = parseInt(page) ? parseInt(page) : 0;
-    Bill.find({ createdBy: id })
-      .sort({ "createdDate": -1 })
-      // .limit(perPage)
-      // .skip(perPage * pageCount)
-      .then(bills => {
-        resolve({ error: false, result: bills });
-      }).catch(err => {
-        reject({ error: true, message: err });
-      })
-  });
+    return new Promise(function (resolve, reject) {
+        // let perPage = parseInt(limit) ? parseInt(limit) : 10;
+        // let pageCount = parseInt(page) ? parseInt(page) : 0;
+        Bill.find({ createdBy: id })
+            .sort({ "createdDate": -1 })
+            .populate('projectName')
+            .populate('client')
+            .exec(function (err, story) {
+                if (err) {
+                    reject({ error: true, message: err });
+                }
+                else {
+                    resolve({ error: false, result: bills });
+
+                }
+            });
+    });
 }
 
 
@@ -61,18 +65,18 @@ exports.getBill = function (id) {
  * no response value expected for this operation
  **/
 exports.deleteBill = function (id) {
-  return new Promise(function (resolve, reject) {
-    Bill.findOneAndRemove({ _id: id }, (error, bill) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-      if (bill)
-        resolve({ error: false, result: bill });
-      else
-        resolve({ error: true, message: "No such bill found" })
-    })
-  });
+    return new Promise(function (resolve, reject) {
+        Bill.findOneAndRemove({ _id: id }, (error, bill) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            if (bill)
+                resolve({ error: false, result: bill });
+            else
+                resolve({ error: true, message: "No such bill found" })
+        })
+    });
 }
 
 
@@ -87,21 +91,21 @@ exports.deleteBill = function (id) {
  * no response value expected for this operation
  **/
 exports.updateBill = function (id, body) {
-  return new Promise(function (resolve, reject) {
-    body.updatedBy = body.userId;
-    body.updatedDate = Date.now();
-    Bill.findOneAndUpdate({ _id: id }, { $set: body }, { new: true, password: 0 }, (error, client) => {
-      console.log(client);
-      console.log(error)
-      if (error) {
-        reject(error);
-        return;
-      }
-      if (bill)
-        resolve({ error: false, result:bill  });
-      else
-        resolve({ error: true, message: "No such bill found" })
-    })
-  });
+    return new Promise(function (resolve, reject) {
+        body.updatedBy = body.userId;
+        body.updatedDate = Date.now();
+        Bill.findOneAndUpdate({ _id: id }, { $set: body }, { new: true, password: 0 }, (error, client) => {
+            console.log(client);
+            console.log(error)
+            if (error) {
+                reject(error);
+                return;
+            }
+            if (bill)
+                resolve({ error: false, result: bill });
+            else
+                resolve({ error: true, message: "No such bill found" })
+        })
+    });
 }
 
